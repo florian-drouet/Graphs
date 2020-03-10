fcefa7f01bc20e28da3ddd1b410669cee82b4130
FlorianDrouet46 <florian.drouet78@gmail.com>
2020-03-10 22:28:30 +0100

    Adding Kruskal algorithm and plotly visualization

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from matplotlib import pyplot as plt
+from graph import graph
+from plotly_graphs import plotly_visualization
+from kruskal import kruskal
 
-graph = edges()
-graph.make_vertices(50,1000)
-graph.make_edges()
-graph.make_coordinates()
-plt.scatter(graph.x_coordinates, graph.y_coordinates)
-plt.show()
+if __name__ == "__main__":
+    #random_graph = graph()
+    #random_graph.make_random_vertices(50,1000)
+    #random_graph.make_edges()
+    #random_graph.make_coordinates()
+    #plt.scatter(random_graph.x_coordinates, random_graph.y_coordinates)
+
+    vertices_list = ((1,2), (2,5), (3,4), (3,3), (0,1), (4,2))
+    graph = graph()
+    graph.make_vertices(vertices_list)
+    graph.make_edges()
+    graph.make_coordinates()
+    kruskal_edges = kruskal(graph.edges, list(graph.vertices.keys()))[0]
+    print(kruskal_edges)
+    plotly_visualization(graph, kruskal_edges=kruskal_edges)
+    #plt.scatter(graph.x_coordinates, graph.y_coordinates)
+    #plt.show()
